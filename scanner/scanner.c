b9095a1ba2c63c080c58eadfc08e1a210704e6f8
Gudmundur Heimisson <gudmundur.heimisson@gmail.com>
2020-05-13 23:58:04 -0700

    Add comments and some memory management

--- scanner/scanner.c
+++ scanner/scanner.c
@@ -9,23 +9,33 @@ const char BLUEZ_BUS[] = "org.bluez";
 const char BLUEZ_ADAPTER_INTERFACE[] = "org.bluez.Adapter1";
 const char BLUEZ_ADAPTER_PATH[] = "/org/bluez/hci0";
 
+// Used to get float values from the raw bytes of the manufacturer data
 union float_bytes {
   float value;
   uint8_t bytes[sizeof(float)];
 };
-
+// Used to get unsigned long values from the raw bytes of the manufacturer data
 union ulong_bytes {
   unsigned long value;
   uint8_t bytes[sizeof(unsigned long)];
 };
 
+/*
+ * The manufacturer data property of the bluez device interface is in a weird
+ * data structure, this function unpacks it into a byte array.
+ */
 void get_manufacturer_data(GVariant* manufacturer_data_dict,  /* in */
                            uint16_t* manufacturer_id,  /* out */
                            uint8_t** data,  /* out */
                            size_t* data_len  /* out */) {
+  // For some reason the manufacturer data is a dictionary of manufacturer
+  // ID to manufacturer data, even though there can only ever be one entry.
+  // First, get the only entry of the dictionary.
   GVariant* man_data = g_variant_get_child_value(manufacturer_data_dict, 0);
+  // Unpack the manufacturer ID and variant containing the data
   GVariant* bytes_variant;
   g_variant_get(man_data, "{qv}", manufacturer_id, &bytes_variant);
+  // Unpack the variant containing the data into a byte array
   GVariantIter bytes_iter;
   g_variant_iter_init(&bytes_iter, bytes_variant);
   GVariant* byte_variant;
@@ -37,6 +47,10 @@ void get_manufacturer_data(GVariant* manufacturer_data_dict,  /* in */
   }
 }
 
+/**
+ * Parses the raw bytes of the manufacturer data into the actual
+ * data values and output them.
+ */
 void process_hygro_data(uint8_t* data, size_t data_len) {
   union ulong_bytes millis;
   union float_bytes temp;
@@ -54,6 +68,10 @@ void process_hygro_data(uint8_t* data, size_t data_len) {
   g_printf("battery: %.2f \n", battery.value);
 }
 
+/**
+ * Convenience function for parsing and displaying manufacturer data from
+ * the hygrometer.
+ */
 void process_manufacturer_data(GVariant* manufacturer_data_variant) {
   uint16_t manufacturer_id;
   uint8_t* data;
@@ -63,27 +81,42 @@ void process_manufacturer_data(GVariant* manufacturer_data_variant) {
                         &data,
                         &data_len);
   process_hygro_data(data, data_len);
+  free(data);
 }
 
+/**
+ * Callback for changes to the hygrometer devices.
+ */
 void on_device_properties_changed(GDBusProxy* device,
                                   GVariant* changed_properties,
                                   GStrv invalidated_properties,
                                   gpointer user_data) {
+  // Iterate through the properties to get the manufacturer data
   GVariantIter* props_iter;
   g_variant_get(changed_properties, "a{sv}", &props_iter);
   char* prop_name;
   GVariant* prop_value;
   while (g_variant_iter_loop(props_iter, "{sv}", &prop_name, &prop_value)) {
     if (g_str_equal(prop_name, "ManufacturerData")) {
+      // Process the manufacturer data
       process_manufacturer_data(prop_value);
     }
   }
+  // g_variant_iter_loop frees prop_name and prop_value
   g_variant_iter_free(props_iter);
 }
 
+/**
+ * Callback for new device discovery.
+ *
+ * Receives a GDBusObject, checks to see if it is a particular device,
+ * then processes the manufacturer data and attaches a callback to it.
+ *
+ */
 void on_device_manager_object_added(GDBusObjectManager* device_manager,
                                     GDBusObject* object,
                                     gpointer user_data) {
+  // Check to see that it's a device and not some other interface
   const gchar* object_path = g_dbus_object_get_object_path(object);
   GDBusInterface* device_interface = g_dbus_object_manager_get_interface(
                                        device_manager,  /* object manager */
@@ -91,8 +124,12 @@ void on_device_manager_object_added(GDBusObjectManager* device_manager,
                                        BLUEZ_DEVICE_INTERFACE  /* interface name */
                                      );
   if (device_interface == NULL) {
+    // Not a device, do nothing
     return;
   }
+  g_object_unref(device_interface);
+
+  // It's a device, get a proxy for it
   GError* error = NULL;
   GDBusProxy* device = g_dbus_proxy_new_for_bus_sync(
                          G_BUS_TYPE_SYSTEM,  /* bus type */
@@ -104,31 +141,61 @@ void on_device_manager_object_added(GDBusObjectManager* device_manager,
                          NULL,  /* cancellable */
                          &error
                        );
+  if (device == NULL) {
+    // This should never happen because we checked that it's a device earlier
+    // but not a fatal error if it does somehow
+    return;
+  }
+
+  // Get the device name to see if we want to process it
   GVariant* alias_variant = g_dbus_proxy_get_cached_property(
                               device,  /* proxy */
                               "Alias"  /* property name */
                             );
   const gchar* alias = g_variant_get_string(alias_variant, NULL  /* length */);
-  if (strncmp("Gummi", alias, strlen("Gummi")) == 0) {
+  // Only want to process this one device
+  if (g_str_equal("Gummi's Hygrometer", alias)) {
+    // Get and process the manufacturer data for the sensor reading
     GVariant* manufacturer_data_dict = g_dbus_proxy_get_cached_property(
                                          device,  /* proxy */
                                          "ManufacturerData"  /* property name */
                                        );
     process_manufacturer_data(manufacturer_data_dict);
+    g_variant_unref(manufacturer_data_dict);
+    // Attach a callback to the device properties so that if the manufacturer
+    // data changes we can reprocess it
     g_signal_connect(
       device, 
       "g-properties-changed",
       G_CALLBACK(on_device_properties_changed),
       NULL
     );
-    g_variant_unref(manufacturer_data_dict);
   }
+  g_variant_unref(alias_variant);
 }
 
 int main(void) {
   GMainLoop* loop = g_main_loop_new(NULL, FALSE);
 
+  // Get the default bluetooth adapter to do device scanning
   GError* error = NULL;
+  GDBusProxy* adapter = g_dbus_proxy_new_for_bus_sync(
+                          G_BUS_TYPE_SYSTEM,  /* bus type */
+                          G_DBUS_PROXY_FLAGS_NONE,  /* flags */
+                          NULL,  /* info */
+                          BLUEZ_BUS,  /* name */
+                          BLUEZ_ADAPTER_PATH,  /* object path */
+                          BLUEZ_ADAPTER_INTERFACE,  /* interface name */
+                          NULL,  /* cancellable */
+                          &error  /* error */
+                        );
+  if (adapter == NULL) {
+    g_fprintf(stderr, error -> message);
+    return 1;
+  }
+
+  // Get the bluez bus to listen for new devices being discovered
+  error = NULL;
   GDBusObjectManager* device_manager = g_dbus_object_manager_client_new_for_bus_sync(
                                          G_BUS_TYPE_SYSTEM,  /* bus type */
                                          G_DBUS_OBJECT_MANAGER_CLIENT_FLAGS_NONE,  /* flags */
@@ -141,30 +208,19 @@ int main(void) {
                                          &error  /* error */
                                        );
   if (device_manager == NULL) {
-    g_print(error->message);
+    g_fprintf(stderr, error->message);
     return 1;
   }
 
-
-  error = NULL;
-  GDBusProxy* adapter = g_dbus_proxy_new_for_bus_sync(
-                          G_BUS_TYPE_SYSTEM,  /* bus type */
-                          G_DBUS_PROXY_FLAGS_NONE,  /* flags */
-                          NULL,  /* info */
-                          BLUEZ_BUS,  /* name */
-                          BLUEZ_ADAPTER_PATH,  /* object path */
-                          BLUEZ_ADAPTER_INTERFACE,  /* interface name */
-                          NULL,  /* cancellable */
-                          &error  /* error */
-                        );
-
-  // Check to see if device already exists
+  // Check to see if device already exists before starting discovery
   GList* devices = g_dbus_object_manager_get_objects(device_manager);
+  // GList is a linked list
   for (GList* d = devices; d != NULL; d = d->next) {
     GDBusObject* device = (GDBusObject*) d->data;
     on_device_manager_object_added(device_manager, device, NULL);
   }
 
+  // Add a listener for new device discovery
   g_signal_connect(
     device_manager,  /* instance */
     "object-added",  /* detailed signal */
@@ -172,6 +228,7 @@ int main(void) {
     NULL  /* data */
   );
 
+  // Start device discovery
   GVariant* start_discover_variant = g_dbus_proxy_call_sync(
                                       adapter,  /* proxy */
                                       "StartDiscovery",  /* method name */
@@ -182,28 +239,13 @@ int main(void) {
                                       &error /* error */
                                      );
   if (start_discover_variant == NULL) {
-    g_print(error->message);
+    g_fprintf(stderr, error->message);
     return 1;
   }
   g_variant_unref(start_discover_variant);
 
+  // Run main loop forever, don't need to call stop discovery
   g_main_loop_run(loop);
-
-  error = NULL;
-  GVariant* stop_discover_variant = g_dbus_proxy_call_sync(
-                                      adapter,  /* proxy */
-                                      "StopDiscovery",  /* method name */
-                                      NULL,  /* parameters */
-                                      G_DBUS_CALL_FLAGS_NONE,  /* flags */
-                                      -1,  /* timeout_msec */
-                                      NULL,  /* cancellable */
-                                      &error  /* error */
-                                    );
-  if (stop_discover_variant == NULL) {
-    g_print(error -> message);
-    return 1;
-  }
-  g_variant_unref(stop_discover_variant);
 
   return 0;
 }
